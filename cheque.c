b3aed12a2947dc96d0f8f2e0b9f3bf8034d551b4
siweh <sanemadesi@gmail.com>
2019-02-18 16:00:45 +0200

    Pay cheque calculations

--- cheque.c
+++ cheque.c
@@ -3,8 +3,7 @@
 int main()
 {
 	float salary,tax,gross;
-	int weeks,month;
-	month = 30;
+	int weeks;
 
 	printf("\nHow many times did you work this month in weeks?: ");
 	scanf("%d",&weeks);
@@ -12,10 +11,10 @@ int main()
 	printf("\nPlease enter your salary for a week:R");
 	scanf("%f",&salary);
 
-	printf("\nYour basic salary before deductions is %.2f",salary);
+	printf("\nYour basic salary before deductions is %.2f",salary* weeks);
 
-	tax = salary - (10/100) + weeks + month;
-	gross = salary + tax + weeks + month;
+	tax = salary - (10/100);
+	gross = salary + tax;
 
 	printf("\nYour tax is %2.f",tax);
 
